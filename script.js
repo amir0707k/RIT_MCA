7acd7c053d54a4a015f0fd31ab79830887937d46
amir0707k <amirk0707@gmail.com>
2024-02-26 21:12:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+// // String concatenation
+// let firstName = "Mohit"
+// let lastName = "Chandola"
+// // ",',`
+// // +
+// console.log("Hi, my name is " + firstName + " "+ lastName)
+
+// // ,
+// console.log("Hi, my name is", firstName, lastName);
+// // `
+// console.log(`Hi, my name is ${firstName} ${lastName}`)
+
+// Number
+// let x = 10;
+// console.log(x)
+
+// let y = 10.11
+// console.log(y)
+
+// BigInt
+// let x = 2423423424242343242424242423432n;
+
+// console.log(x)
+
+// undefined
+// let x ;
+// console.log(x)
+// x = 10
+// console.log(x)
+
+// null
+// const x = null
+// if(!null){
+//     console.log("Value is not null")
+// }
+
+// Boolean
+// const a = true;
+// // const b = 4
+
+// console.log(a)
+
+// var obj = {
+//     firstName:"Abc",
+//     lastName:"def",
+//     age:24,
+//     isWorking:true
+// }
+
+// console.log(obj)
+// console.log(obj.firstName)
+
+// let, var and Const
+
+        // scope       Hoisting        Reassignment
+// var                                      yes
+// let                                      yes
+// const                                    No
+
+// let x = 10;
+// console.log(x)
+// x = 22;
+// console.log(x)
+
+// var x = 10;
+// console.log(x);
+// x =20;
+// console.log(x)
+
+const x = 10;
+console.log(x);
+x = 20;
+console.log(x);
